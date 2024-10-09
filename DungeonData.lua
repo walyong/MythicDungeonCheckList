693043be29a467bcde15576cea744315355e3daf
WaLyong Cho <walyong.cho@gmail.com>
2024-10-09 16:43:12 +0900

    update DungeonData

--- DungeonData.lua
+++ DungeonData.lua
@@ -6,7 +6,7 @@ MythicCheckListData = {}
 -- 기본 던전 설정 (변경되지 않는 기본값)
 MythicCheckListData.DefaultDungeonSettings = {
     ["메아리의 도시 아라카라"] = {
-        maxMeleeUnits = 5,
+        maxMeleeUnits = 2,  -- 최대 근접 유닛을 2로 설정
         maxRangedUnits = 5,
         mustHaveCurse = 0,
         mustHaveMagic = 0,
@@ -27,26 +27,26 @@ MythicCheckListData.DefaultDungeonSettings = {
         maxMeleeUnits = 5,
         maxRangedUnits = 5,
         mustHaveCurse = 0,
-        mustHaveMagic = 1,    -- 마법 해제 필요
+        mustHaveMagic = 0,    -- 필요 마법 해제를 0으로 설정
         mustHavePoison = 0,
-        mustHaveDisease = 1,  -- 질병 해제 필요
-        mustHaveEnrage = 0
+        mustHaveDisease = 0,  -- 필요 질병 해제를 0으로 설정
+        mustHaveEnrage = 1    -- 필요 격노 해제를 1로 설정
     },
     ["새벽인도자호"] = {
         maxMeleeUnits = 5,
         maxRangedUnits = 5,
         mustHaveCurse = 0,
-        mustHaveMagic = 1,    -- 마법 해제 필요
-        mustHavePoison = 1,   -- 독 해제 필요
+        mustHaveMagic = 1,
+        mustHavePoison = 1,
         mustHaveDisease = 0,
-        mustHaveEnrage = 1    -- 격노 해제 필요
+        mustHaveEnrage = 1
     },
     ["티르너 사이드의 안개"] = {
         maxMeleeUnits = 5,
         maxRangedUnits = 5,
-        mustHaveCurse = 1,    -- 저주 해제 필요
+        mustHaveCurse = 1,
         mustHaveMagic = 0,
-        mustHavePoison = 1,   -- 독 해제 필요
+        mustHavePoison = 1,
         mustHaveDisease = 0,
         mustHaveEnrage = 0
     },
@@ -54,16 +54,16 @@ MythicCheckListData.DefaultDungeonSettings = {
         maxMeleeUnits = 5,
         maxRangedUnits = 5,
         mustHaveCurse = 0,
-        mustHaveMagic = 1,    -- 마법 해제 필요
+        mustHaveMagic = 1,
         mustHavePoison = 0,
-        mustHaveDisease = 1,  -- 질병 해제 필요
-        mustHaveEnrage = 1    -- 격노 해제 필요
+        mustHaveDisease = 1,
+        mustHaveEnrage = 1
     },
     ["보랄러스 공성전"] = {
-        maxMeleeUnits = 3,
+        maxMeleeUnits = 3,    -- 최대 근접 유닛을 3으로 설정
         maxRangedUnits = 5,
         mustHaveCurse = 0,
-        mustHaveMagic = 2,    -- 마법 해제 2명 필요
+        mustHaveMagic = 2,    -- 필요 마법 해제를 2로 설정
         mustHavePoison = 0,
         mustHaveDisease = 0,
         mustHaveEnrage = 0
@@ -75,6 +75,6 @@ MythicCheckListData.DefaultDungeonSettings = {
         mustHaveMagic = 0,
         mustHavePoison = 0,
         mustHaveDisease = 0,
-        mustHaveEnrage = 1    -- 격노 해제 필요
+        mustHaveEnrage = 1    -- 필요 격노 해제를 1로 설정
     }
 }
